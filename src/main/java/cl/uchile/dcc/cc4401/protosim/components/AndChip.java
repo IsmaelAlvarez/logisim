a7e43d4f9bf6e46a9c3e544a7dc1331e4c5df9a5
moishernandez <moishernandez1990@gmail.com>
2015-11-27 20:58:09 -0300

    se modifica la lógica del componente AND de modo que tenga que ser conectado a una pila para que funcione

--- src/main/java/cl/uchile/dcc/cc4401/protosim/components/AndChip.java
+++ src/main/java/cl/uchile/dcc/cc4401/protosim/components/AndChip.java
@@ -33,12 +33,15 @@ public class AndChip extends InstanceFactory {
         // Upper ports
         ports.add(new Port(0, 0, Port.INPUT, Breadboard.PORT_WIDTH));
         ports.add(new Port(10, 0, Port.INPUT, Breadboard.PORT_WIDTH));
-        ports.add(new Port(20, 0, Port.OUTPUT, Breadboard.PORT_WIDTH));
+        ports.add(new Port(20, 0, Port.INPUT, Breadboard.PORT_WIDTH));
+        ports.add(new Port(30, 0, Port.OUTPUT, Breadboard.PORT_WIDTH));
+        
 
         // Lower ports
         ports.add(new Port(0, 30, Port.INPUT, Breadboard.PORT_WIDTH));
         ports.add(new Port(10, 30, Port.INPUT, Breadboard.PORT_WIDTH));
         ports.add(new Port(20, 30, Port.OUTPUT, Breadboard.PORT_WIDTH));
+        ports.add(new Port(30, 30, Port.INPUT, Breadboard.PORT_WIDTH));
 
         setPorts(ports);
     }
@@ -60,42 +63,50 @@ public class AndChip extends InstanceFactory {
 
         // Chip
         g.setColor(Color.black);
-        g.fillRect(x - 2, y + 5, 24, 20);
+        g.fillRect(x - 2, y + 5, 34, 20);
 
         // Text
         g.setColor(Color.white);
         g.setFont(new Font("Courier", Font.BOLD, 9));
-        g.drawString("AND", x + 2, y + 17);
+        g.drawString("AND", x + 8, y + 17);
+        g.drawString("+", x-1, y + 12);
+        g.drawString("-", x + 25, y + 24);
 
         // Pins
         g.setColor(Color.gray);
         g.fillRect(x - 2, y, 4, 5);
         g.fillRect(x + 8, y, 4, 5);
         g.fillRect(x + 18, y, 4, 5);
+        g.fillRect(x + 28, y, 4, 5);
 
         g.fillRect(x - 2, y + 25, 4, 5);
         g.fillRect(x + 8, y + 25, 4, 5);
         g.fillRect(x + 18, y + 25, 4, 5);
+        g.fillRect(x + 28, y + 25, 4, 5);
         
         painter.drawPorts();
     }
 
     @Override
     public Bounds getOffsetBounds(AttributeSet attrs) {
-        return Bounds.create(0, 0, 20, 30);
+        return Bounds.create(0, 0, 30, 30);
     }
 
     @Override
     public void propagate(InstanceState state) {
-    	setOutputValue(state, 0, 1, 2);
-    	setOutputValue(state, 3, 4, 5);
+    	setOutputValue(state, 0, 7, 1, 2, 3);
+    	setOutputValue(state, 0, 7, 4, 5, 6);
     }
     
-    private void setOutputValue(InstanceState state, int portAIndex, int portBIndex, int portOutIndex) {
-        Value valueA = state.getPort(portAIndex);
+    private void setOutputValue(InstanceState state,int vcc, int ground, int portAIndex, int portBIndex, int portOutIndex) {
+        Value valueVCC = state.getPort(vcc);
+        Value valueGround = state.getPort(ground);
+    	Value valueA = state.getPort(portAIndex);
         Value valueB = state.getPort(portBIndex);
         
-        Value result;
+        Value result= Value.UNKNOWN;
+        
+        if (valueVCC.isUnknown() == false && valueGround.isUnknown()==false && valueVCC.toIntValue() == ProtoValue.TRUE && valueGround.toIntValue() == ProtoValue.FALSE){
         if (valueA.isUnknown() || valueB.isUnknown()) {
         	result = Value.createKnown(BitWidth.create(Breadboard.PORT_WIDTH), 0);
         } else {
@@ -107,7 +118,7 @@ public class AndChip extends InstanceFactory {
             else
             	voltageR = ProtoValue.FALSE;
             result = Value.createKnown(BitWidth.create(Breadboard.PORT_WIDTH), voltageR);
-        }
+        }}
 
         state.setPort(portOutIndex, result, Breadboard.DELAY);
     }
