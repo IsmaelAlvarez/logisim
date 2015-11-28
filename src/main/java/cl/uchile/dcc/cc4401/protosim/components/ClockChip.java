9f92da1adf3120e04ddbef73d5107e2351ebcb8c
theUnknown <mariocesarcornejo@gmail.com>
2015-11-28 01:05:49 -0300

    issue #21 reloj terminado, solo funciona conectado a una pila

--- src/main/java/cl/uchile/dcc/cc4401/protosim/components/ClockChip.java
+++ src/main/java/cl/uchile/dcc/cc4401/protosim/components/ClockChip.java
@@ -9,7 +9,6 @@ import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import com.cburch.logisim.circuit.CircuitState;
 import com.cburch.logisim.comp.Component;
 import com.cburch.logisim.data.Attribute;
@@ -33,198 +32,222 @@ import com.cburch.logisim.std.wiring.Pin;
 import com.cburch.logisim.std.wiring.Probe;
 import com.cburch.logisim.util.Icons;
 
+import cl.uchile.dcc.cc4401.protosim.libraries.ProtoValue;
+
 public class ClockChip extends InstanceFactory {
-    
-    public static InstanceFactory FACTORY = new ClockChip();
-    
-    private List<Port> ports;
 
-    public ClockChip() {
-        super("Clock Chip", getFromLocale("clockComponent"));
-        this.setIcon(Icons.getIcon("protosimComponentClock.svg")); 
-        
-        ports = new ArrayList<Port>();
+	public static InstanceFactory FACTORY = new ClockChip();
 
-        // Upper ports
-        ports.add(new Port(20, 0, Port.OUTPUT, Breadboard.PORT_WIDTH));
-        ports.add(new Port(0, 0, Port.INPUT, Breadboard.PORT_WIDTH));
-        ports.add(new Port(10, 0, Port.INPUT, Breadboard.PORT_WIDTH));
-        
-        // Lower ports
-        ports.add(new Port(0, 30, Port.INPUT, Breadboard.PORT_WIDTH));
-        ports.add(new Port(10, 30, Port.INPUT, Breadboard.PORT_WIDTH));
-        ports.add(new Port(20, 30, Port.OUTPUT, Breadboard.PORT_WIDTH));
+	private List<Port> ports;
 
-        setPorts(ports);
-        
-        setAttributes(new Attribute[] {
-                StdAttr.FACING, Clock.ATTR_HIGH, Clock.ATTR_LOW,
-                StdAttr.LABEL, Pin.ATTR_LABEL_LOC, StdAttr.LABEL_FONT
-            }, new Object[] {
-                Direction.EAST, Integer.valueOf(1), Integer.valueOf(1),
-                "", Direction.WEST, StdAttr.DEFAULT_LABEL_FONT
-            });
-        
-	    setFacingAttribute(StdAttr.FACING);
-	    setInstanceLogger(ClockLogger.class);
-	    setInstancePoker(ClockPoker.class);
-    }
-    
-    @Override
-    public Bounds getOffsetBounds(AttributeSet attrs) {
-        return Bounds.create(0, 0, 20, 30);
-    }
+	public ClockChip() {
+		super("Clock Chip", getFromLocale("clockComponent"));
+		this.setIcon(Icons.getIcon("protosimComponentClock.svg"));
 
-    @Override
-    public void paintInstance(InstancePainter painter) {
-        Location loc = painter.getLocation();
-        int x = loc.getX();
-        int y = loc.getY();
+		ports = new ArrayList<Port>();
 
-        Graphics g = painter.getGraphics();
+		// Upper ports
+		ports.add(new Port(0, 0, Port.INPUT, Breadboard.PORT_WIDTH));
+		ports.add(new Port(10, 0, Port.OUTPUT, Breadboard.PORT_WIDTH));
+		ports.add(new Port(20, 0, Port.OUTPUT, Breadboard.PORT_WIDTH));
 
-        // Chip
-        g.setColor(Color.black);
-        g.fillRect(x - 2, y + 5, 24, 20);
+		// Lower ports
+		ports.add(new Port(0, 30, Port.OUTPUT, Breadboard.PORT_WIDTH));
+		ports.add(new Port(10, 30, Port.OUTPUT, Breadboard.PORT_WIDTH));
+		ports.add(new Port(20, 30, Port.INPUT, Breadboard.PORT_WIDTH));
 
-        // Text
-        g.setColor(Color.white);
-        g.setFont(new Font("Courier", Font.BOLD, 9));
-        g.drawString("CLK", x + 2, y + 17);
+		setPorts(ports);
 
-        // Pins
-        g.setColor(Color.gray);
-        g.fillRect(x - 2, y, 4, 5);
-        g.fillRect(x + 8, y, 4, 5);
-        g.fillRect(x + 18, y, 4, 5);
+		setAttributes(
+				new Attribute[] { StdAttr.FACING, Clock.ATTR_HIGH, Clock.ATTR_LOW, StdAttr.LABEL, Pin.ATTR_LABEL_LOC,
+						StdAttr.LABEL_FONT },
+				new Object[] { Direction.EAST, Integer.valueOf(1), Integer.valueOf(1), "", Direction.WEST,
+						StdAttr.DEFAULT_LABEL_FONT });
 
-        g.fillRect(x - 2, y + 25, 4, 5);
-        g.fillRect(x + 8, y + 25, 4, 5);
-        g.fillRect(x + 18, y + 25, 4, 5);
-        
-        painter.drawPorts();
-    }
-    
+		setFacingAttribute(StdAttr.FACING);
+		setInstanceLogger(ClockLogger.class);
+		setInstancePoker(ClockPoker.class);
+	}
+
+	@Override
+	public Bounds getOffsetBounds(AttributeSet attrs) {
+		return Bounds.create(0, 0, 20, 30);
+	}
+
+	@Override
+	public void paintInstance(InstancePainter painter) {
+		Location loc = painter.getLocation();
+		int x = loc.getX();
+		int y = loc.getY();
+
+		Graphics g = painter.getGraphics();
+
+		// Chip
+		g.setColor(Color.black);
+		g.fillRect(x - 2, y + 5, 24, 20);
+
+		// Text
+		g.setColor(Color.white);
+		g.setFont(new Font("Courier", Font.BOLD, 9));
+		g.drawString("CLK", x + 2, y + 17);
+
+		// Pins
+		g.setColor(Color.gray);
+		g.fillRect(x - 2, y, 4, 5);
+		g.fillRect(x + 8, y, 4, 5);
+		g.fillRect(x + 18, y, 4, 5);
+
+		g.fillRect(x - 2, y + 25, 4, 5);
+		g.fillRect(x + 8, y + 25, 4, 5);
+		g.fillRect(x + 18, y + 25, 4, 5);
+
+		painter.drawPorts();
+	}
+
 	private static class ClockState implements InstanceData, Cloneable {
-	    Value sending = Value.createKnown(BitWidth.create(32), 0);
-	    int clicks = 0;
-	
-	    @Override
-	    public ClockState clone() {
-	        try { return (ClockState) super.clone(); }
-	        catch (CloneNotSupportedException e) { return null; }
-	    }
+		Value sending = Value.createKnown(BitWidth.create(Breadboard.PORT_WIDTH), ProtoValue.FALSE);
+		int clicks = 0;
+
+		@Override
+		public ClockState clone() {
+			try {
+				return (ClockState) super.clone();
+			} catch (CloneNotSupportedException e) {
+				return null;
+			}
+		}
 	}
-	
+
 	public static class ClockLogger extends InstanceLogger {
-	    @Override
-	    public String getLogName(InstanceState state, Object option) {
-	        return state.getAttributeValue(StdAttr.LABEL);
-	    }
-	
-	    @Override
-	    public Value getLogValue(InstanceState state, Object option) {
-	        ClockState s = getState(state);
-	        return s.sending;
-	    }
+		@Override
+		public String getLogName(InstanceState state, Object option) {
+			return state.getAttributeValue(StdAttr.LABEL);
+		}
+
+		@Override
+		public Value getLogValue(InstanceState state, Object option) {
+			ClockState s = getState(state);
+			return s.sending;
+		}
 	}
-	
+
 	public static class ClockPoker extends InstancePoker {
-	    boolean isPressed = true;
-	
-	    @Override
-	    public void mousePressed(InstanceState state, MouseEvent e) {
-	        isPressed = isInside(state, e);
-	    }
-	
-	    @Override
-	    public void mouseReleased(InstanceState state, MouseEvent e) {
-	        if (isPressed && isInside(state, e)) {
-	            ClockState myState = (ClockState) state.getData();
-	            myState.sending = myState.sending.not();
-	            myState.clicks++;
-	            state.fireInvalidated();
-	        }
-	        isPressed = false;
-	    }
-	
-	    private boolean isInside(InstanceState state, MouseEvent e) {
-	        Bounds bds = state.getInstance().getBounds();
-	        return bds.contains(e.getX(), e.getY());
-	    }
+		boolean isPressed = true;
+
+		@Override
+		public void mousePressed(InstanceState state, MouseEvent e) {
+			isPressed = isInside(state, e);
+		}
+
+		@Override
+		public void mouseReleased(InstanceState state, MouseEvent e) {
+			if (isPressed && isInside(state, e)) {
+				ClockState myState = (ClockState) state.getData();
+				myState.sending = myState.sending.not();
+				myState.clicks++;
+				state.fireInvalidated();
+			}
+			isPressed = false;
+		}
+
+		private boolean isInside(InstanceState state, MouseEvent e) {
+			Bounds bds = state.getInstance().getBounds();
+			return bds.contains(e.getX(), e.getY());
+		}
 	}
-	
+
 	//
 	// methods for instances
 	//
 	@Override
 	protected void configureNewInstance(Instance instance) {
-	    instance.addAttributeListener();
-	   //instance.setPorts(new Port[] { new Port(20, 0, Port.OUTPUT, Breadboard.PORT_WIDTH)});
-	    configureLabel(instance);
+		instance.addAttributeListener();
+		// instance.setPorts(new Port[] { new Port(20, 0, Port.OUTPUT,
+		// Breadboard.PORT_WIDTH)});
+		configureLabel(instance);
 	}
-	
+
 	@Override
 	protected void instanceAttributeChanged(Instance instance, Attribute<?> attr) {
-	    if (attr == Pin.ATTR_LABEL_LOC) {
-	        configureLabel(instance);
-	    } else if (attr == StdAttr.FACING) {
-	        instance.recomputeBounds();
-	        configureLabel(instance);
-	    }
+		if (attr == Pin.ATTR_LABEL_LOC) {
+			configureLabel(instance);
+		} else if (attr == StdAttr.FACING) {
+			instance.recomputeBounds();
+			configureLabel(instance);
+		}
 	}
-	
+
+	private boolean isEnergized(InstanceState state, int vcc, int ground) {
+		Value valueVCC = state.getPort(vcc);
+		Value valueGround = state.getPort(ground);
+		if (valueVCC.isFullyDefined() && valueGround.isFullyDefined() && valueVCC.toIntValue() == ProtoValue.TRUE
+				&& valueGround.toIntValue() == ProtoValue.FALSE) {
+			return true;
+		}
+		return false;
+	}
+
 	@Override
 	public void propagate(InstanceState state) {
-	    Value val = state.getPort(0);
-	    ClockState q = getState(state);
-	    // ignore if no change
-	    if (!val.equals(q.sending)) {
-	        state.setPort(0, q.sending, 1);
-	    }
+		Value val = state.getPort(2);
+		ClockState q = getState(state);
+		// ignore if no change
+		if (!val.equals(q.sending) && isEnergized(state, 0, 5)) {
+			state.setPort(1, q.sending, 1);
+			state.setPort(2, q.sending, 1);
+			state.setPort(3, q.sending, 1);
+			state.setPort(4, q.sending, 1);
+		}
+		if (!isEnergized(state, 0, 5)) {
+			state.setPort(1, Value.createUnknown(BitWidth.create(Breadboard.PORT_WIDTH)), 1);
+			state.setPort(2, Value.createUnknown(BitWidth.create(Breadboard.PORT_WIDTH)), 1);
+			state.setPort(3, Value.createUnknown(BitWidth.create(Breadboard.PORT_WIDTH)), 1);
+			state.setPort(4, Value.createUnknown(BitWidth.create(Breadboard.PORT_WIDTH)), 1);
+		}
 	}
-	
+
 	//
 	// package methods
 	//
 	public static boolean tick(CircuitState circState, int ticks, Component comp) {
-	    AttributeSet attrs = comp.getAttributeSet();
-	    int durationHigh = attrs.getValue(Clock.ATTR_HIGH).intValue();
-	    int durationLow = attrs.getValue(Clock.ATTR_LOW).intValue();
-	    ClockState state = (ClockState) circState.getData(comp);
-	    if (state == null) {
-	        state = new ClockState();
-	        circState.setData(comp, state);
-	    }
-	    boolean curValue = ticks % (durationHigh + durationLow) < durationLow;
-	    if (state.clicks % 2 == 1) {
-	        curValue = !curValue;
-	    }
-	
-	    Value desired = (curValue ? Value.createKnown(BitWidth.create(32), 0) : Value.createKnown(BitWidth.create(32), 0).not());
-	    if (!state.sending.equals(desired)) {
-	        state.sending = desired;
-	        Instance.getInstanceFor(comp).fireInvalidated();
-	        return true;
-	    }
-	    return false;
+		AttributeSet attrs = comp.getAttributeSet();
+		int durationHigh = attrs.getValue(Clock.ATTR_HIGH).intValue();
+		int durationLow = attrs.getValue(Clock.ATTR_LOW).intValue();
+		ClockState state = (ClockState) circState.getData(comp);
+		if (state == null) {
+			state = new ClockState();
+			circState.setData(comp, state);
+		}
+		boolean curValue = ticks % (durationHigh + durationLow) < durationLow;
+		if (state.clicks % 2 == 1) {
+			curValue = !curValue;
+		}
+
+		Value desired = (curValue ? Value.createKnown(BitWidth.create(Breadboard.PORT_WIDTH), ProtoValue.FALSE)
+				: Value.createKnown(BitWidth.create(Breadboard.PORT_WIDTH), ProtoValue.TRUE));
+		if (!state.sending.equals(desired)) {
+			state.sending = desired;
+			Instance.getInstanceFor(comp).fireInvalidated();
+			return true;
+		}
+		return false;
 	}
-	
+
 	//
 	// private methods
 	//
 	private void configureLabel(Instance instance) {
-	    Direction facing = instance.getAttributeValue(StdAttr.FACING);
-	    Direction labelLoc = instance.getAttributeValue(Pin.ATTR_LABEL_LOC);
-	    Probe.configureLabel(instance, labelLoc, facing);
+		Direction facing = instance.getAttributeValue(StdAttr.FACING);
+		Direction labelLoc = instance.getAttributeValue(Pin.ATTR_LABEL_LOC);
+		Probe.configureLabel(instance, labelLoc, facing);
 	}
-	
+
 	private static ClockState getState(InstanceState state) {
-	    ClockState ret = (ClockState) state.getData();
-	    if (ret == null) {
-	        ret = new ClockState();
-	        state.setData(ret);
-	    }
-	    return ret;
+		ClockState ret = (ClockState) state.getData();
+		if (ret == null) {
+			ret = new ClockState();
+			state.setData(ret);
+		}
+		return ret;
 	}
 }
