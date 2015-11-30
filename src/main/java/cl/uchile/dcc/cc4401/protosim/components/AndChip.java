9c402f576f78dff605e9e7ca47be20998c5a2e12
zero1712 <zero_gibran@hotmail.com>
2015-11-30 20:56:07 -0300

    Se respeta el estandar comercial 7404 en la compuerta AND

--- src/main/java/cl/uchile/dcc/cc4401/protosim/components/AndChip.java
+++ src/main/java/cl/uchile/dcc/cc4401/protosim/components/AndChip.java
@@ -35,13 +35,18 @@ public class AndChip extends InstanceFactory {
         ports.add(new Port(10, 0, Port.INPUT, Breadboard.PORT_WIDTH));
         ports.add(new Port(20, 0, Port.INPUT, Breadboard.PORT_WIDTH));
         ports.add(new Port(30, 0, Port.OUTPUT, Breadboard.PORT_WIDTH));
-        
+        ports.add(new Port(40, 0, Port.INPUT, Breadboard.PORT_WIDTH));
+        ports.add(new Port(50, 0, Port.INPUT, Breadboard.PORT_WIDTH));
+        ports.add(new Port(60, 0, Port.OUTPUT, Breadboard.PORT_WIDTH));
 
         // Lower ports
         ports.add(new Port(0, 30, Port.INPUT, Breadboard.PORT_WIDTH));
         ports.add(new Port(10, 30, Port.INPUT, Breadboard.PORT_WIDTH));
         ports.add(new Port(20, 30, Port.OUTPUT, Breadboard.PORT_WIDTH));
         ports.add(new Port(30, 30, Port.INPUT, Breadboard.PORT_WIDTH));
+        ports.add(new Port(40, 30, Port.INPUT, Breadboard.PORT_WIDTH));
+        ports.add(new Port(50, 30, Port.OUTPUT, Breadboard.PORT_WIDTH));
+        ports.add(new Port(60, 30, Port.INPUT, Breadboard.PORT_WIDTH));
 
         setPorts(ports);
     }
@@ -63,14 +68,14 @@ public class AndChip extends InstanceFactory {
 
         // Chip
         g.setColor(Color.black);
-        g.fillRect(x - 2, y + 5, 34, 20);
+        g.fillRect(x - 2, y + 5, 64, 20);
 
         // Text
         g.setColor(Color.white);
         g.setFont(new Font("Courier", Font.BOLD, 9));
-        g.drawString("AND", x + 8, y + 17);
+        g.drawString("7408 AND", x + 11, y + 17);
         g.drawString("+", x-1, y + 12);
-        g.drawString("-", x + 25, y + 24);
+        g.drawString("-", x + 57, y + 24);
 
         // Pins
         g.setColor(Color.gray);
@@ -78,24 +83,32 @@ public class AndChip extends InstanceFactory {
         g.fillRect(x + 8, y, 4, 5);
         g.fillRect(x + 18, y, 4, 5);
         g.fillRect(x + 28, y, 4, 5);
+        g.fillRect(x + 38, y, 4, 5);
+        g.fillRect(x + 48, y, 4, 5);
+        g.fillRect(x + 58, y, 4, 5);
 
         g.fillRect(x - 2, y + 25, 4, 5);
         g.fillRect(x + 8, y + 25, 4, 5);
         g.fillRect(x + 18, y + 25, 4, 5);
         g.fillRect(x + 28, y + 25, 4, 5);
+        g.fillRect(x + 38, y + 25, 4, 5);
+        g.fillRect(x + 48, y + 25, 4, 5);
+        g.fillRect(x + 58, y + 25, 4, 5);
         
         painter.drawPorts();
     }
 
     @Override
     public Bounds getOffsetBounds(AttributeSet attrs) {
-        return Bounds.create(0, 0, 30, 30);
+        return Bounds.create(0, 0, 60, 30);
     }
 
     @Override
     public void propagate(InstanceState state) {
-    	setOutputValue(state, 0, 7, 1, 2, 3);
-    	setOutputValue(state, 0, 7, 4, 5, 6);
+    	setOutputValue(state, 0, 13, 1, 2, 3);
+    	setOutputValue(state, 0, 13, 4, 5, 6);
+    	setOutputValue(state, 0, 13, 7, 8, 9);
+    	setOutputValue(state, 0, 13, 10, 11, 12);
     }
     
     private void setOutputValue(InstanceState state,int vcc, int ground, int portAIndex, int portBIndex, int portOutIndex) {
