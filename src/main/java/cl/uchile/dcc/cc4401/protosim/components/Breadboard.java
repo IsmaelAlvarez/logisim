a626f602c355edb3a2ea5d5ce8724c97ac772016
theUnknown <mariocesarcornejo@gmail.com>
2015-11-30 21:54:25 -0300

    se aumentaron los pines de la protoboard

--- src/main/java/cl/uchile/dcc/cc4401/protosim/components/Breadboard.java
+++ src/main/java/cl/uchile/dcc/cc4401/protosim/components/Breadboard.java
@@ -69,14 +69,16 @@ public class Breadboard extends InstanceFactory {
 		}
 		pinGroup++;
 		for (int i = 0; i <= 64 * 10; i += 10) {
-			Port port1 = new Port(10 + i, 50, Port.INOUT, PORT_WIDTH);
-			Port port2 = new Port(10 + i, 50 + 10, Port.INOUT, PORT_WIDTH);
-			Port port3 = new Port(10 + i, 50 + 20, Port.INOUT, PORT_WIDTH);
-			Port port4 = new Port(10 + i, 50 + 30, Port.INOUT, PORT_WIDTH);
-			Port port5 = new Port(10 + i, 110, Port.INOUT, PORT_WIDTH);
-			Port port6 = new Port(10 + i, 110 + 10, Port.INOUT, PORT_WIDTH);
-			Port port7 = new Port(10 + i, 110 + 20, Port.INOUT, PORT_WIDTH);
-			Port port8 = new Port(10 + i, 110 + 30, Port.INOUT, PORT_WIDTH);
+			Port port1 = new Port(10 + i, 40, Port.INOUT, PORT_WIDTH);
+			Port port2 = new Port(10 + i, 40 + 10, Port.INOUT, PORT_WIDTH);
+			Port port3 = new Port(10 + i, 40 + 20, Port.INOUT, PORT_WIDTH);
+			Port port4 = new Port(10 + i, 40 + 30, Port.INOUT, PORT_WIDTH);
+			Port port5 = new Port(10 + i, 40 + 40, Port.INOUT, PORT_WIDTH);
+			Port port6 = new Port(10 + i, 110, Port.INOUT, PORT_WIDTH);
+			Port port7 = new Port(10 + i, 110 + 10, Port.INOUT, PORT_WIDTH);
+			Port port8 = new Port(10 + i, 110 + 20, Port.INOUT, PORT_WIDTH);
+			Port port9 = new Port(10 + i, 110 + 30, Port.INOUT, PORT_WIDTH);
+			Port port10 = new Port(10 + i, 110 + 40, Port.INOUT, PORT_WIDTH);
 			ports.add(port1);
 			ports.add(port2);
 			ports.add(port3);
@@ -85,15 +87,19 @@ public class Breadboard extends InstanceFactory {
 			ports.add(port6);
 			ports.add(port7);
 			ports.add(port8);
+			ports.add(port9);
+			ports.add(port10);
 			connected.put(port1, pinGroup);
 			connected.put(port2, pinGroup);
 			connected.put(port3, pinGroup);
 			connected.put(port4, pinGroup);
-			pinGroup++;
 			connected.put(port5, pinGroup);
+			pinGroup++;
 			connected.put(port6, pinGroup);
 			connected.put(port7, pinGroup);
 			connected.put(port8, pinGroup);
+			connected.put(port9, pinGroup);
+			connected.put(port10, pinGroup);
 			pinGroup++;
 		}	
 	}
