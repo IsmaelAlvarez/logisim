1307ed4f3e1c7dbaeffae9d2f753ac3313a0e986
Diego Madariaga <d.madariagaroman@gmail.com>
2016-05-01 22:42:00 -0300

    Se agrega clase para la creación del componente "Chip Integrado 555"

---
+++ src/main/java/cl/uchile/dcc/cc4401/protosim/components/Timer555IntegratedChip.java
@@ -0,0 +1,26 @@
+package cl.uchile.dcc.cc4401.protosim.components;
+
+import com.cburch.logisim.instance.InstanceFactory;
+import com.cburch.logisim.instance.InstancePainter;
+import com.cburch.logisim.instance.InstanceState;
+
+public class Timer555IntegratedChip extends InstanceFactory{
+
+	public Timer555IntegratedChip(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void paintInstance(InstancePainter painter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void propagate(InstanceState state) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
