43018e8475458f7d18fe58b5392a9ea20ad7345d
Vicente Rotman <vrotmanh@gmail.com>
2016-05-31 13:29:05 -0400

    Funciones para limpiar y reducir el grafo para el calculo de resistencia equvalente entre 2 componentes cualesquiera

--- src/main/java/cl/uchile/dcc/cc4401/protosim/AllComponents.java
+++ src/main/java/cl/uchile/dcc/cc4401/protosim/AllComponents.java
@@ -5,6 +5,7 @@ import com.cburch.logisim.comp.Component;
 import com.cburch.logisim.std.io.Io;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by vicenterotman on 4/14/16.
@@ -35,6 +36,82 @@ public class AllComponents {
         return calculateResistanceRecursive(c1,c2);
     }
 
+    //Reduce el grafo
+    private ArrayList<ComponentConnection> reduceGraph(AnalogComponent c1, ArrayList<ComponentConnection> graph){
+        HashMap<Integer, Integer[]> dic = new HashMap<Integer, Integer[]>();
+
+        if(graph.size() <= 2)
+            return graph;
+
+        //Recorre el grafo y obtiene las cantidades de from y to de cada componente
+        for(ComponentConnection cc : graph){
+            //Sumo 1 al from
+            if(dic.containsKey(cc.getFrom().getId()))
+                dic.get(cc.getFrom().getId())[0]++;
+            else{
+                Integer[] i = new Integer[2];
+                i[0] = 1;
+                i[1] = 0;
+                dic.put(cc.getFrom().getId(), i);
+            }
+
+            //Sumo 1 al to
+            if(dic.containsKey(cc.getTo().getId()))
+                dic.get(cc.getTo().getId())[1]++;
+            else{
+                Integer[] i = new Integer[2];
+                i[0] = 0;
+                i[1] = 1;
+                dic.put(cc.getTo().getId(), i);
+            }
+        }
+
+        //Recorre dic y va determinando cuales conexiones agregar y cuales juntar
+        for(Integer k1  : dic.keySet()){
+            //Relacion 1-1
+            if((dic.get(k1)[0] == 1) && (dic.get(k1)[1] == 1)){
+                for(Integer k2  : dic.keySet()){
+                    //Relacion 1-1
+                    if((dic.get(k2)[0] == 1) && (dic.get(k2)[1] == 1)){
+                        //Es serie
+                        for(ComponentConnection cc : graph){
+                            if(((cc.getFrom().getId() == k1) && (cc.getTo().getId() == k2))
+                                    || ((cc.getFrom().getId() == k2) && (cc.getTo().getId() == k1))){
+
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+        return graph;
+    }
+
+    //Limpia el grafo, dejando solo la parte entre los 1 componente y la fuente
+    private ArrayList<ComponentConnection> cleanGraph(AnalogComponent c1){
+        ArrayList<ComponentConnection> newGraph = new ArrayList<ComponentConnection>();
+
+        //Recorro el grafo
+        for(ComponentConnection cc : getGraph()){
+
+            //Si la conección va desde c1 a la fuente no continuo recorriendo y añado al grafo
+            if((cc.getFrom().getId() == c1.getId()) && (cc.getTo().getId() == getVoltageGenerator().getId())){
+                newGraph.add(cc);
+                return newGraph;
+            }
+
+            //Si la conección comienza del nodo c1, agrego la llamada recursiva
+            if(cc.getFrom().getId() == c1.getId()){
+                newGraph.add(cc);
+                newGraph.addAll(cleanGraph(cc.getTo()));
+            }
+        }
+
+        return newGraph;
+    }
+
     //Calcula la resistencia entre 2 puntos
     private double calculateResistanceRecursive(AnalogComponent c1, AnalogComponent c2){
         double res = 0;
@@ -43,7 +120,7 @@ public class AllComponents {
         for(ComponentConnection cc : getGraph()){
             if(cc.getTo().getId() == c1.getId()) froms++;
         }
-        
+
         for(ComponentConnection cc : getGraph()){
             if(cc.getFrom().getId() == c1.getId()){
                 //Si esta conectado a c2 retorno su resistencia
