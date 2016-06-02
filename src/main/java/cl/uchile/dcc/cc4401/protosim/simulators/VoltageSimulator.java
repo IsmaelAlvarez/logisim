199dd0ad19bd01f989ef4f59a6d052cebcbc1b12
Nicolas Caracci <nicolascaracci@gmail.com>
2016-06-02 13:17:00 -0400

    Simulacion funciona excepto el caso especial y manda el msje de quemado con la caida de voltaje producida por el componente

--- src/main/java/cl/uchile/dcc/cc4401/protosim/simulators/VoltageSimulator.java
+++ src/main/java/cl/uchile/dcc/cc4401/protosim/simulators/VoltageSimulator.java
@@ -14,10 +14,67 @@ import cl.uchile.dcc.cc4401.protosim.ComponentConnection;
 
 public class VoltageSimulator implements AnalogSimulator {
 
-	private HashMap<Integer, ArrayList<AnalogComponent>> connected_to;
+	private HashMap<Integer, FromToComponent> connected_to;
 	private double current;
 	private AnalogComponent vol_gen;
 	
+	private class FromToComponent {
+		
+		private AnalogComponent ac;
+		private ArrayList<AnalogComponent> in = new ArrayList<AnalogComponent>();
+		private ArrayList<AnalogComponent> out = new ArrayList<AnalogComponent>();
+		private double current = 0;
+		private int counter_in = 0;
+		private int current_in = 0;
+		
+		public FromToComponent(AnalogComponent ac) {
+			
+			this.ac = ac;
+		}
+		
+		public AnalogComponent getAnalogComponent() {
+			
+			return ac;
+		}
+		
+		public boolean addIn(AnalogComponent ac) {
+			
+			boolean resp = in.add(ac);
+			if (resp) {
+				counter_in++;
+			}
+			return resp;
+		}
+		
+		public boolean addOut(AnalogComponent ac) {
+			
+			return out.add(ac);
+		}
+		
+		public ArrayList<AnalogComponent> getIn() {
+			
+			return in;
+		}
+		
+		public ArrayList<AnalogComponent> getOut() {
+			
+			return out;
+		}
+		
+		public boolean countArrival(double corriente_entrada) {
+			
+			current += corriente_entrada;
+			current_in++;
+			if (current_in == counter_in)
+				return true;
+			return false;
+		}
+		
+		public double getCurrent() {
+			
+			return current;
+		}
+	}
 	
 	@Override
 	public void simulate(ArrayList<ComponentConnection> graph) {
@@ -27,14 +84,19 @@ public class VoltageSimulator implements AnalogSimulator {
 		double voltage = ((Voltage) vol_gen.getAttrs().getValue(Io.ATTR_VOLTAGE)).getVoltage();
 		current = voltage / total_resistance;
 		
-		connected_to = new HashMap<Integer, ArrayList<AnalogComponent>>();
+		connected_to = new HashMap<Integer, FromToComponent>();
 		
 		// Genera hasmap id -> lista componentes salida
 		for (ComponentConnection cc : graph) {
 			if (!connected_to.containsKey(cc.getFrom().getId())) {
-				connected_to.put(cc.getFrom().getId(), new ArrayList<AnalogComponent>());
+				connected_to.put(cc.getFrom().getId(), new FromToComponent(cc.getFrom()));
 			}
-			connected_to.get(cc.getFrom().getId()).add(cc.getTo());
+			connected_to.get(cc.getFrom().getId()).addOut(cc.getTo());
+			
+			if (!connected_to.containsKey(cc.getTo().getId())) {
+				connected_to.put(cc.getTo().getId(), new FromToComponent(cc.getTo()));
+			}
+			connected_to.get(cc.getTo().getId()).addIn(cc.getFrom());
 		}
 		
 		// Inicia la simulacion con la bateria
@@ -43,31 +105,40 @@ public class VoltageSimulator implements AnalogSimulator {
 	
 	public void start_propagate(double voltage) {
 		
-		for (AnalogComponent cc : connected_to.get(vol_gen.getId())) {
+		for (AnalogComponent cc : connected_to.get(vol_gen.getId()).getOut()) {
 			if (cc.getId() == vol_gen.getId())
 				continue;
 			double output_current = voltage / AllComponents.getMyInstance().calculateEqResistance(cc, vol_gen);
+			System.out.println(Integer.toString(cc.getId()) + " llega " + Double.toString(output_current));
 			propagate_voltage(cc, output_current, voltage);
 		}
 	}
 	
 	public void propagate_voltage(AnalogComponent comp, double input_current, double input_voltage) {
 		
-		// Ver si el componente se quema
-		comp.checkIfBurns(input_voltage);
+
+		// Saca el componente
+		FromToComponent componente = connected_to.get(comp.getId());
+
+		// Si faltan en llegar marca y se devuelve
+		if (!componente.countArrival(input_current)) {
+			return;
+		}
 		
+		double total_current = componente.getCurrent();
 		// V - I_1 * R_1 = V_2
-		// Si comp es la bateria, input_voltage = output_voltage
-		double output_voltage = input_voltage - input_current * comp.getRes();
+		double output_voltage = input_voltage - total_current * comp.getRes();
+		
+		// Ver si el componente se quema
+		comp.checkIfBurns(total_current * comp.getRes());
+		
 		System.out.println(Integer.toString(comp.getId()) + " voltaje salida: " + Double.toString(output_voltage));
 		
-		ArrayList<AnalogComponent> to = connected_to.get(comp.getId());
-		if (to == null)
-			return;
-		for (AnalogComponent cc : to) {
+		for (AnalogComponent cc : componente.getOut()) {
 			if (cc.getId() == vol_gen.getId())
 				continue;
 			double output_current = output_voltage / AllComponents.getMyInstance().calculateEqResistance(cc, vol_gen);
+			System.out.println(Integer.toString(cc.getId()) + " llega " + Double.toString(output_current));
 			propagate_voltage(cc, output_current, output_voltage);
 		}
 	}
